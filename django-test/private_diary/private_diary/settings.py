ec89b87f6bb09c1858618a7405d59d9168f01ebf
flat000 <hiramoto.00@gmail.com>
2021-01-24 16:09:52 +0900

    20210124_2

--- django-test/private_diary/private_diary/settings.py
+++ django-test/private_diary/private_diary/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# Build paths inside the project like this: BASE_DIR / 'subdir'.  Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -165,3 +165,7 @@ LOGGING = {
         },
     }
 }
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
